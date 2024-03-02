dd981f7d61b91a707c5a89ab0470c5b085877cac
Thiago_Batista_da_Silva_Oliveira <gmoliveira69@gmail.com>
2024-03-02 15:41:25 -0300

    fix: native base bug

--- App.tsx
+++ App.tsx
@@ -1,28 +1,30 @@
-import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import { useFonts, Roboto_400Regular } from "@expo-google-fonts/roboto";
 import {
   useFonts as useBalooFonts,
   BalooDa2_700Bold,
-} from '@expo-google-fonts/baloo-da-2'
-import { StatusBar, Text } from 'react-native';
-import { Loading } from './src/components';
-import { AppProvider } from './src/context';
+} from "@expo-google-fonts/baloo-da-2";
+import { StatusBar, Text } from "react-native";
+import { Loading } from "./src/components";
+import { AppProvider } from "./src/context";
 
 export default function App() {
   const [robotoFontsLoaded] = useFonts({ Roboto_400Regular });
   const [balooFontsLoaded] = useBalooFonts({ BalooDa2_700Bold });
 
-
-  if (!robotoFontsLoaded || !balooFontsLoaded) {
-    return <Loading />;
-  }
   return (
-     <AppProvider>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-       <Text>Hello, World</Text>
-     </AppProvider>
+    <AppProvider>
+      {!robotoFontsLoaded || !balooFontsLoaded ? (
+        <Loading />
+      ) : (
+        <>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <Text>Hello, World</Text>
+        </>
+      )}
+    </AppProvider>
   );
 }
